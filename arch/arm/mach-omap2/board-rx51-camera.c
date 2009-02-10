e5b262b597cc9c38211f5c1d1fef5d1c402d6488
Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
2009-02-10 17:33:19 +0200

    RX-51: camera: Add board code
    Squashed from the following upstream commits:
    
    rx51: Camera Kconfig entries
    rx51: Add camera base files
    rx51: Just rx51 camera
    rx51: Drop mt9p012t and tcm8330md sensors
    rx51: change board files for smia-sensor
    rx51: Don't use VAUX3 for new boards anymore
    rx51: Tell if sensor power up failed
    rx51: Do not set xclk in omap34xxcam_sensor_config
    rx51: update configure_interface and calculate average pixel clock
    rx51: limit maximum LED flash power to 320 mA
    rx51: update the ISP API on camera board file
    rx51: Interpret system_rev properly
    rx51: adjust LED flash constraints to prevent overheating
    rx51: Decrement wait_hs_vs by one
    rx51: Port ad5820 platform data to v4l2_subdev
    rx51: Port et8ek8 platform data to v4l2_subdev
    rx51: Port smia-sensor platform data to v4l2_subdev
    rx51: Remove fake platform data
    rx51: Add a release callback to the platform device
    rx51: Add omap34xxcam platform data
    rx51: Header files have moved from mach to plat.
    rx51: vpclk renamed vpclk_div
    rx51: Add omap3isp_device pointer to platform data.
    rx51: Correct TWL register names
    rx51 cam: Add ISP platform data
    rx51 cam: Don't set removed isp_interface_config fields
    rx51: Make it compile again.
    rx51: Make camera board code compile
    rx51: camera: set bus type correctly to CCP2
    rx51 cam: Update to the new OMAP3 camera platform data
    rx51: Update to the new OMAP3 ISP platform data
    rx51: Update to the new OMAP3 ISP platform data
    rx51 cam: Remove hardcoded I2C module names
    rx51: Fix Kconfig breakage for camera
    rx51: Select VIDEO_MACH_RX51 in MACH_NOKIA_RX51
    rx51 cam: Calculate pixelclk for secondary sensor
    rx51 cam: Update to the new OMAP3 ISP platform data
    omap3: Export omap3_init_camera() instead of omap3isp_device
    rx51 cam: Don't directly depend on ISP driver
    rx51 cam: Board files compiled staticly now
    rx51 cam: Drop support for old I2C configuration
    rx51 cam: Drop power control from board file
    rx51 cam: Add xshutdown handling for camera modules
    rx51 cam: Add CONTROL_CSIRXFE configuration
    rx51 peripherals: add VANA regulator supply definitions for camera
    rx51 cam: Replace the configure_interface() arguments
    rx51 cam: Port to 2.6.37
    rx51: omap3isp.h header moved
    rx51: Use standard units for intensities
    
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
    Signed-off-by: David Cohen <david.cohen@nokia.com>
    Signed-off-by: Ameya Palande <ameya.palande@nokia.com>
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>

---
+++ arch/arm/mach-omap2/board-rx51-camera.c
@@ -0,0 +1,409 @@
+/*
+ * arch/arm/mach-omap2/board-rx51-camera.c
+ *
+ * Copyright (C) 2008 Nokia Corporation
+ *
+ * Contact: Sakari Ailus <sakari.ailus@nokia.com>
+ *          Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#include <linux/i2c.h>
+#include <linux/i2c/twl.h>
+#include <linux/delay.h>
+#include <linux/mm.h>
+#include <linux/platform_device.h>
+#include <linux/videodev2.h>
+
+#include <asm/gpio.h>
+
+#include "../../../drivers/media/platform/omap3isp/isp.h"
+#include "../../../drivers/media/platform/omap3isp/ispreg.h"
+#include "../../../drivers/media/i2c/smia/et8ek8.h"
+#include "../../../drivers/media/i2c/smia/smia-sensor.h"
+
+#include <media/ad5820.h>
+#include <media/adp1653.h>
+#include <media/smiaregs.h>
+
+#include "control.h"
+#include "devices.h"
+#include "omap34xx.h"
+
+#define ADP1653_GPIO_ENABLE	88	/* Used for resetting ADP1653 */
+#define ADP1653_GPIO_INT	167	/* Fault interrupt */
+#define ADP1653_GPIO_STROBE	126	/* Pin used in cam_strobe mode ->
+					 * control using ISP drivers */
+
+#define STINGRAY_RESET_GPIO	102
+#define ACMELITE_RESET_GPIO	97	/* Used also to MUX between cameras */
+
+#define RX51_CAMERA_STINGRAY	(1 << 0)
+#define RX51_CAMERA_LENS	(1 << 1)
+#define RX51_CAMERA_ACMELITE	(1 << 2)
+
+#define RX51_CAMERA_PRIMARY	(RX51_CAMERA_STINGRAY | RX51_CAMERA_LENS)
+#define RX51_CAMERA_SECONDARY	RX51_CAMERA_ACMELITE
+
+static DEFINE_MUTEX(rx51_camera_mutex);
+static unsigned int rx51_camera_xshutdown;
+
+static int rx51_camera_set_xshutdown(unsigned int which, int set)
+{
+	unsigned int new = rx51_camera_xshutdown;
+	int ret = 0;
+
+	mutex_lock(&rx51_camera_mutex);
+
+	if (set)
+		new |= which;
+	else
+		new &= ~which;
+
+	/* The primary and secondary cameras can't be powered on at the same
+	 * time.
+	 */
+	if ((new & RX51_CAMERA_PRIMARY) && (new & RX51_CAMERA_SECONDARY)) {
+		ret = -EBUSY;
+		goto out;
+	}
+
+	if ((rx51_camera_xshutdown & RX51_CAMERA_PRIMARY) !=
+	    (new & RX51_CAMERA_PRIMARY))
+		gpio_set_value(STINGRAY_RESET_GPIO,
+			       new & RX51_CAMERA_PRIMARY ? 1 : 0);
+
+	if ((rx51_camera_xshutdown & RX51_CAMERA_SECONDARY) !=
+	    (new & RX51_CAMERA_SECONDARY))
+		gpio_set_value(ACMELITE_RESET_GPIO,
+			       new & RX51_CAMERA_SECONDARY ? 1 : 0);
+
+	rx51_camera_xshutdown = new;
+
+out:
+	mutex_unlock(&rx51_camera_mutex);
+	return ret;
+}
+
+static void __init rx51_stingray_init(void)
+{
+	if (gpio_request(STINGRAY_RESET_GPIO, "stingray reset") != 0) {
+		printk(KERN_INFO "%s: unable to acquire Stingray reset gpio\n",
+		       __FUNCTION__);
+		return;
+	}
+
+	/* XSHUTDOWN off, reset  */
+	gpio_direction_output(STINGRAY_RESET_GPIO, 0);
+	gpio_set_value(STINGRAY_RESET_GPIO, 0);
+}
+
+static void __init rx51_acmelite_init(void)
+{
+	if (gpio_request(ACMELITE_RESET_GPIO, "acmelite reset") != 0) {
+		printk(KERN_INFO "%s: unable to acquire Acme Lite reset gpio\n",
+		       __FUNCTION__);
+		return;
+	}
+
+	/* XSHUTDOWN off, reset  */
+	gpio_direction_output(ACMELITE_RESET_GPIO, 0);
+	gpio_set_value(ACMELITE_RESET_GPIO, 0);
+}
+
+static int __init rx51_adp1653_init(void)
+{
+	int err;
+
+	err = gpio_request(ADP1653_GPIO_ENABLE, "adp1653 enable");
+	if (err) {
+		printk(KERN_ERR ADP1653_NAME
+		       " Failed to request EN gpio\n");
+		err = -ENODEV;
+		goto err_omap_request_gpio;
+	}
+
+	err = gpio_request(ADP1653_GPIO_INT, "adp1653 interrupt");
+	if (err) {
+		printk(KERN_ERR ADP1653_NAME " Failed to request IRQ gpio\n");
+		err = -ENODEV;
+		goto err_omap_request_gpio_2;
+	}
+
+	err = gpio_request(ADP1653_GPIO_STROBE, "adp1653 strobe");
+	if (err) {
+		printk(KERN_ERR ADP1653_NAME
+		       " Failed to request STROBE gpio\n");
+		err = -ENODEV;
+		goto err_omap_request_gpio_3;
+	}
+
+	gpio_direction_output(ADP1653_GPIO_ENABLE, 0);
+	gpio_direction_input(ADP1653_GPIO_INT);
+	gpio_direction_output(ADP1653_GPIO_STROBE, 0);
+
+	return 0;
+
+err_omap_request_gpio_3:
+	gpio_free(ADP1653_GPIO_INT);
+
+err_omap_request_gpio_2:
+	gpio_free(ADP1653_GPIO_ENABLE);
+
+err_omap_request_gpio:
+	return err;
+}
+
+static int __init rx51_camera_hw_init(void)
+{
+	int rval;
+
+	mutex_init(&rx51_camera_mutex);
+
+	rval = rx51_adp1653_init();
+	if (rval)
+		return rval;
+
+	rx51_stingray_init();
+	rx51_acmelite_init();
+
+	return 0;
+}
+
+/*
+ *
+ * Stingray
+ *
+ */
+
+#define STINGRAY_XCLK		ISP_XCLK_A
+
+static int rx51_stingray_set_xclk(struct v4l2_subdev *subdev, int hz)
+{
+	struct isp_device *isp = v4l2_dev_to_isp_device(subdev->v4l2_dev);
+
+	if (!isp)
+		return 1;
+
+	isp->platform_cb.set_xclk(isp, hz, STINGRAY_XCLK);
+
+	return 0;
+}
+
+static int rx51_stingray_set_xshutdown(struct v4l2_subdev *subdev, int set)
+{
+	int ret;
+
+	ret = rx51_camera_set_xshutdown(RX51_CAMERA_STINGRAY, set);
+	if (ret == 0 && set) {
+		/* CONTROL_CSIRXFE
+		 * Data/strobe, enable transceiver, disable reset
+		 */
+		omap_ctrl_writel(OMAP343X_CSIB_RESET | OMAP343X_CSIB_PWRDNZ |
+			    OMAP343X_CSIB_SELFORM,
+			    OMAP343X_CONTROL_CSIRXFE);
+	}
+
+	return ret;
+}
+
+static struct et8ek8_platform_data rx51_et8ek8_platform_data = {
+	.set_xclk		= rx51_stingray_set_xclk,
+	.set_xshutdown		= rx51_stingray_set_xshutdown,
+};
+
+/*
+ *
+ * AD5820
+ *
+ */
+
+static int rx51_ad5820_set_xshutdown(struct v4l2_subdev *subdev, int set)
+{
+	return rx51_camera_set_xshutdown(RX51_CAMERA_LENS, set);
+}
+
+static struct ad5820_platform_data rx51_ad5820_platform_data = {
+	.set_xshutdown		= rx51_ad5820_set_xshutdown,
+};
+
+/*
+ *
+ * ADP1653
+ *
+ */
+
+static int rx51_adp1653_power(struct v4l2_subdev *subdev, int on)
+{
+	gpio_set_value(ADP1653_GPIO_ENABLE, on);
+	if (on) {
+		/* Some delay is apparently required. */
+		udelay(20);
+	}
+
+	return 0;
+}
+
+static struct adp1653_platform_data rx51_adp1653_platform_data = {
+	.power			 = rx51_adp1653_power,
+	/* Must be limited to 500 ms in RX-51 */
+	.max_flash_timeout	 = 500000,		/* us */
+	/* Must be limited to 320 mA in RX-51 B3 and newer hardware */
+	.max_flash_intensity	 = ADP1653_FLASH_INTENSITY_REG_TO_mA(19),
+	/* Must be limited to 50 mA in RX-51 */
+	.max_torch_intensity	 = ADP1653_FLASH_INTENSITY_REG_TO_mA(1),
+	.max_indicator_intensity = ADP1653_INDICATOR_INTENSITY_REG_TO_uA(
+		ADP1653_REG_OUT_SEL_ILED_MAX),
+};
+
+/*
+ *
+ * Acmelite
+ *
+ */
+
+#define ACMELITE_XCLK		ISP_XCLK_A
+
+static int rx51_acmelite_set_xclk(struct v4l2_subdev *subdev, int hz)
+{
+	struct isp_device *isp = v4l2_dev_to_isp_device(subdev->v4l2_dev);
+
+	if (!isp)
+		return 1;
+
+	isp->platform_cb.set_xclk(isp, hz, ACMELITE_XCLK);
+
+	return 0;
+}
+
+static int rx51_acmelite_set_xshutdown(struct v4l2_subdev *subdev, int set)
+{
+	int ret;
+
+	ret = rx51_camera_set_xshutdown(RX51_CAMERA_ACMELITE, set);
+	if (ret == 0 && set) {
+		/* CONTROL_CSIRXFE
+		 * Data/clock, enable transceiver, disable reset
+		 */
+		omap_ctrl_writel(OMAP343X_CSIB_RESET | OMAP343X_CSIB_PWRDNZ,
+			    OMAP343X_CONTROL_CSIRXFE);
+	}
+
+	return ret;
+}
+
+static struct smia_sensor_platform_data rx51_smia_sensor_platform_data = {
+	.set_xclk		= rx51_acmelite_set_xclk,
+	.set_xshutdown		= rx51_acmelite_set_xshutdown,
+};
+
+/*
+ *
+ * Init it all
+ *
+ */
+
+#define ET8EK8_I2C_BUS_NUM		3
+#define AD5820_I2C_BUS_NUM		3
+#define ADP1653_I2C_BUS_NUM		2
+#define SMIA_SENSOR_I2C_BUS_NUM		2
+
+static struct i2c_board_info rx51_camera_i2c_devices[] = {
+	{
+		I2C_BOARD_INFO(ET8EK8_NAME, ET8EK8_I2C_ADDR),
+		.platform_data = &rx51_et8ek8_platform_data,
+	},
+	{
+		I2C_BOARD_INFO(AD5820_NAME, AD5820_I2C_ADDR),
+		.platform_data = &rx51_ad5820_platform_data,
+	},
+	{
+		I2C_BOARD_INFO(ADP1653_NAME, ADP1653_I2C_ADDR),
+		.platform_data = &rx51_adp1653_platform_data,
+	},
+	{
+		I2C_BOARD_INFO(SMIA_SENSOR_NAME, SMIA_SENSOR_I2C_ADDR),
+		.platform_data = &rx51_smia_sensor_platform_data,
+	},
+};
+
+static struct isp_subdev_i2c_board_info rx51_camera_primary_subdevs[] = {
+	{
+		.board_info = &rx51_camera_i2c_devices[0],
+		.i2c_adapter_id = ET8EK8_I2C_BUS_NUM,
+	},
+	{
+		.board_info = &rx51_camera_i2c_devices[1],
+		.i2c_adapter_id = AD5820_I2C_BUS_NUM,
+	},
+	{
+		.board_info = &rx51_camera_i2c_devices[2],
+		.i2c_adapter_id = ADP1653_I2C_BUS_NUM,
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_subdev_i2c_board_info rx51_camera_secondary_subdevs[] = {
+	{
+		.board_info = &rx51_camera_i2c_devices[3],
+		.i2c_adapter_id = SMIA_SENSOR_I2C_BUS_NUM,
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_v4l2_subdevs_group rx51_camera_subdevs[] = {
+	{
+		.subdevs = rx51_camera_primary_subdevs,
+		.interface = ISP_INTERFACE_CCP2B_PHY1,
+		.bus = { .ccp2 = {
+			.strobe_clk_pol		= 0,
+			.crc			= 1,
+			.ccp2_mode		= 1,
+			.phy_layer		= 1,
+			.vpclk_div		= 1,
+		} },
+	},
+	{
+		.subdevs = rx51_camera_secondary_subdevs,
+		.interface = ISP_INTERFACE_CCP2B_PHY1,
+		.bus = { .ccp2 = {
+			.strobe_clk_pol		= 0,
+			.crc			= 1,
+			.ccp2_mode		= 1,
+			.phy_layer		= 1,
+			.vpclk_div		= 1,
+		} },
+	},
+	{ NULL, 0, },
+};
+
+static struct isp_platform_data rx51_isp_platform_data = {
+	.subdevs = rx51_camera_subdevs,
+};
+
+void __init rx51_camera_init(void)
+{
+	if (rx51_camera_hw_init()) {
+		printk(KERN_WARNING "%s: Unable to initialize camera\n",
+		       __func__);
+		return;
+	}
+
+	if (omap3_init_camera(&rx51_isp_platform_data) < 0)
+		printk(KERN_WARNING "%s: Unable to register camera platform "
+		       "device\n", __func__);
+}
