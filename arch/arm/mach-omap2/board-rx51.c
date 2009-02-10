db2fd63101eabd7dbfe8ef2bac083fe7671d9116
Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
2009-02-10 17:33:19 +0200

    RX-51: camera: Add board code
    Squashed from the following upstream commits:
    
    rx51: Camera Kconfig entries
    rx51: Add camera base files
    rx51: Just rx51 camera
    rx51: Drop mt9p012t and tcm8330md sensors
    rx51: change board files for smia-sensor
    rx51: Don't use VAUX3 for new boards anymore
    rx51: Tell if sensor power up failed
    rx51: Do not set xclk in omap34xxcam_sensor_config
    rx51: update configure_interface and calculate average pixel clock
    rx51: limit maximum LED flash power to 320 mA
    rx51: update the ISP API on camera board file
    rx51: Interpret system_rev properly
    rx51: adjust LED flash constraints to prevent overheating
    rx51: Decrement wait_hs_vs by one
    rx51: Port ad5820 platform data to v4l2_subdev
    rx51: Port et8ek8 platform data to v4l2_subdev
    rx51: Port smia-sensor platform data to v4l2_subdev
    rx51: Remove fake platform data
    rx51: Add a release callback to the platform device
    rx51: Add omap34xxcam platform data
    rx51: Header files have moved from mach to plat.
    rx51: vpclk renamed vpclk_div
    rx51: Add omap3isp_device pointer to platform data.
    rx51: Correct TWL register names
    rx51 cam: Add ISP platform data
    rx51 cam: Don't set removed isp_interface_config fields
    rx51: Make it compile again.
    rx51: Make camera board code compile
    rx51: camera: set bus type correctly to CCP2
    rx51 cam: Update to the new OMAP3 camera platform data
    rx51: Update to the new OMAP3 ISP platform data
    rx51: Update to the new OMAP3 ISP platform data
    rx51 cam: Remove hardcoded I2C module names
    rx51: Fix Kconfig breakage for camera
    rx51: Select VIDEO_MACH_RX51 in MACH_NOKIA_RX51
    rx51 cam: Calculate pixelclk for secondary sensor
    rx51 cam: Update to the new OMAP3 ISP platform data
    omap3: Export omap3_init_camera() instead of omap3isp_device
    rx51 cam: Don't directly depend on ISP driver
    rx51 cam: Board files compiled staticly now
    rx51 cam: Drop support for old I2C configuration
    rx51 cam: Drop power control from board file
    rx51 cam: Add xshutdown handling for camera modules
    rx51 cam: Add CONTROL_CSIRXFE configuration
    rx51 peripherals: add VANA regulator supply definitions for camera
    rx51 cam: Replace the configure_interface() arguments
    rx51 cam: Port to 2.6.37
    rx51: omap3isp.h header moved
    rx51: Use standard units for intensities
    
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
    Signed-off-by: David Cohen <david.cohen@nokia.com>
    Signed-off-by: Ameya Palande <ameya.palande@nokia.com>
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>

--- arch/arm/mach-omap2/board-rx51.c
+++ arch/arm/mach-omap2/board-rx51.c
@@ -80,6 +80,7 @@ static struct platform_device leds_gpio = {
 */
 
 extern void __init rx51_peripherals_init(void);
+extern void __init rx51_camera_init(void);
 
 #ifdef CONFIG_OMAP_MUX
 static struct omap_board_mux board_mux[] __initdata = {
@@ -106,6 +107,7 @@ static void __init rx51_init(void)
 	usb_bind_phy("musb-hdrc.0.auto", 0, "twl4030_usb");
 	usb_musb_init(&musb_board_data);
 	rx51_peripherals_init();
+	rx51_camera_init();
 
 	if (omap_type() == OMAP2_DEVICE_TYPE_SEC) {
 #ifdef CONFIG_ARM_ERRATA_430973
