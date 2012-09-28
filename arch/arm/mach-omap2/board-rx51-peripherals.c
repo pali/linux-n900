b674d87317a27f122227d88628c93dda121dcfb8
Joni Lapilainen <joni.lapilainen@gmail.com>
2012-09-29 00:24:11 +0300

    Fix probe order of SPI devices
    Signed-off-by: Joni Lapilainen <joni.lapilainen@gmail.com>

--- arch/arm/mach-omap2/board-rx51-peripherals.c
+++ arch/arm/mach-omap2/board-rx51-peripherals.c
@@ -76,8 +76,8 @@
 /* list all spi devices here */
 enum {
 	RX51_SPI_WL1251,
-	RX51_SPI_MIPID,		/* LCD panel */
 	RX51_SPI_TSC2005,	/* Touch Controller */
+	RX51_SPI_MIPID,		/* LCD panel */
 };
 
 static struct wl12xx_platform_data wl1251_pdata;
