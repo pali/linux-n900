985f3bdb03410c96bb15379bbd6278a243576b00
Ivaylo Dimitrov <freemangordon@abv.bg>
2013-09-12 22:07:59 +0100

    smiapp: reintroduce set_xshutdown in platform code
    On some hardware (like rx51) xshutdown GPIO has other meanings as well.
    Allow board code to handle that.
    
    Signed-off-by: Ivaylo Dimitrov <freemangordon@abv.bg>

--- drivers/media/i2c/smiapp/smiapp-core.c
+++ drivers/media/i2c/smiapp/smiapp-core.c
@@ -1215,6 +1215,16 @@ static int smiapp_power_on(struct smiapp_sensor *sensor)
 
 	if (gpio_is_valid(sensor->platform_data->xshutdown))
 		gpio_set_value(sensor->platform_data->xshutdown, 1);
+	else {
+		if (sensor->platform_data->set_xshutdown) {
+			rval = sensor->platform_data->set_xshutdown(
+				&sensor->src->sd, 1);
+			if (rval) {
+				dev_err(&client->dev, "sensor xshutdown failed\n");
+				goto out_xshut_fail;
+			}
+		}
+	}
 
 	sleep = SMIAPP_RESET_DELAY(sensor->platform_data->ext_clk);
 	usleep_range(sleep, sleep);
@@ -1325,6 +1335,11 @@ static int smiapp_power_on(struct smiapp_sensor *sensor)
 out_cci_addr_fail:
 	if (gpio_is_valid(sensor->platform_data->xshutdown))
 		gpio_set_value(sensor->platform_data->xshutdown, 0);
+	else {
+		if (sensor->platform_data->set_xshutdown)
+			sensor->platform_data->set_xshutdown(&sensor->src->sd, 0);
+	}
+out_xshut_fail:
 	if (sensor->platform_data->set_xclk)
 		sensor->platform_data->set_xclk(&sensor->src->sd, 0);
 	else
@@ -1351,6 +1366,10 @@ static void smiapp_power_off(struct smiapp_sensor *sensor)
 
 	if (gpio_is_valid(sensor->platform_data->xshutdown))
 		gpio_set_value(sensor->platform_data->xshutdown, 0);
+	else {
+		if (sensor->platform_data->set_xshutdown)
+			sensor->platform_data->set_xshutdown(&sensor->src->sd, 0);
+	}
 	if (sensor->platform_data->set_xclk)
 		sensor->platform_data->set_xclk(&sensor->src->sd, 0);
 	else
@@ -2965,6 +2984,11 @@ static int smiapp_remove(struct i2c_client *client)
 	if (sensor->power_count) {
 		if (gpio_is_valid(sensor->platform_data->xshutdown))
 			gpio_set_value(sensor->platform_data->xshutdown, 0);
+		else {
+			if (sensor->platform_data->set_xshutdown)
+				sensor->platform_data->set_xshutdown(
+					&sensor->src->sd, 0);
+		}
 		if (sensor->platform_data->set_xclk)
 			sensor->platform_data->set_xclk(&sensor->src->sd, 0);
 		else
