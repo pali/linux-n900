bfbd31b828b4fa59c09f40dc2805416d0eeb9d70
Ivaylo Dimitrov <freemangordon@abv.bg>
2013-12-30 18:08:01 +0000

    ARM: OMAPFB: panel-sony-acx565akm: fix missing mutex unlocks
    Commit c37dd677988ca50bc8bc60ab5ab053720583c168 fixes the unbalanced
    unlock in acx565akm_enable but introduces another problem - if
    acx565akm_panel_power_on exits early, the mutex is not unlocked. Fix
    that by unlocking the mutex on early return. Also add mutex protection in
    acx565akm_panel_power_off and remove an unused variable
    
    Signed-off-by: Ivaylo Dimitrov <freemangordon@abv.bg>

--- drivers/video/fbdev/omap2/displays-new/panel-sony-acx565akm.c
+++ drivers/video/fbdev/omap2/displays-new/panel-sony-acx565akm.c
@@ -555,6 +555,7 @@ static int acx565akm_panel_power_on(struct omap_dss_device *dssdev)
 
 	r = in->ops.sdi->enable(in);
 	if (r) {
+		mutex_unlock(&ddata->mutex);
 		pr_err("%s sdi enable failed\n", __func__);
 		return r;
 	}
@@ -566,6 +567,7 @@ static int acx565akm_panel_power_on(struct omap_dss_device *dssdev)
 		gpio_set_value(ddata->reset_gpio, 1);
 
 	if (ddata->enabled) {
+		mutex_unlock(&ddata->mutex);
 		dev_dbg(&ddata->spi->dev, "panel already enabled\n");
 		return 0;
 	}
@@ -596,10 +598,14 @@ static void acx565akm_panel_power_off(struct omap_dss_device *dssdev)
 	struct panel_drv_data *ddata = to_panel_data(dssdev);
 	struct omap_dss_device *in = ddata->in;
 
+	mutex_lock(&ddata->mutex);
+
 	dev_dbg(dssdev->dev, "%s\n", __func__);
 
-	if (!ddata->enabled)
+	if (!ddata->enabled) {
+		mutex_unlock(&ddata->mutex);
 		return;
+	}
 
 	set_display_state(ddata, 0);
 	set_sleep_mode(ddata, 1);
@@ -619,11 +625,13 @@ static void acx565akm_panel_power_off(struct omap_dss_device *dssdev)
 	msleep(100);
 
 	in->ops.sdi->disable(in);
+
+	mutex_unlock(&ddata->mutex);
+
 }
 
 static int acx565akm_enable(struct omap_dss_device *dssdev)
 {
-	struct panel_drv_data *ddata = to_panel_data(dssdev);
 	int r;
 
 	dev_dbg(dssdev->dev, "%s\n", __func__);
