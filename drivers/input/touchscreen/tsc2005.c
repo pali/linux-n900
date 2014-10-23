726d136ff94152be2495847f2a92ee49127ee0e1
Pavel Machek <pavel@ucw.cz>
2014-10-23 17:23:16 +0200

    touchscreen: tsc2005: Fix compilation by replacing strict_strtoul to kstrtoul
    Signed-off-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Pali Rohár <pali.rohar@gmail.com>

--- drivers/input/touchscreen/tsc2005.c
+++ drivers/input/touchscreen/tsc2005.c
@@ -403,7 +403,7 @@ static ssize_t tsc2005_disable_store(struct device *dev,
 	unsigned long val;
 	int error;
 
-	error = strict_strtoul(buf, 10, &val);
+	error = kstrtoul(buf, 10, &val);
 	if (error)
 		return error;
 
